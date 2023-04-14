61db8bb67d26d33d6a1fd1c66041dbbae81b25e3
Paola Ruiz Puentes <pruiz@lambda002.uniandes.edu.co>
2023-04-14 18:03:00 -0400

    Inference

--- planet_inference.sh
+++ planet_inference.sh
@@ -1,6 +1,6 @@
 device=2
 TARGET=ada
-EXPERIMENT='ISBI/PLA-Net/BINARY_'
+EXPERIMENT='/media/SSD5/pruiz/2022-2023/PLA-Net/log/ISBI/PLA-Net/'
 BS=10
 
-CUDA_VISIBLE_DEVICES=$DEVICE python inference.py --freeze_molecule --use_gpu --conv_encode_edge --learn_t --batch_size $BS --binary --use_prot --target $TARGET --save $EXPERIMENT
+CUDA_VISIBLE_DEVICES=$DEVICE python inference.py --freeze_molecule --use_gpu --conv_encode_edge --learn_t --batch_size $BS --binary --use_prot --target $TARGET --inference_path $EXPERIMENT
